ca388bfa174bab50207f8cee1d066cb90f8bad33
Amjad-722 <aliamjad722ee@gmail.com>
2024-07-31 21:47:16 +0500

    create a first todo app

---
+++ src/components/Input.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+  
+    export let type = "text";
+    export let value = "";
+    export let placeholder = "";
+  
+    const dispatch = createEventDispatcher();
+  
+    const handleClick = () => {
+      dispatch('click', { value });
+    };
+  
+    const handleInput = (event) => {
+      value = event.target.value;
+    };
+  </script>
+  
+  <div>
+    <input
+      type={type}
+      value={value}
+      placeholder={placeholder}
+      on:input={handleInput}
+      class="border-4 border-amber-900 align-middle p-2 rounded"
+    />
+    <button
+      on:click={handleClick}
+      class="ml-2 bg-amber-900 text-white p-2 rounded"
+    >
+      Show Text
+    </button>
+  </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Input from '../components/Input.svelte';
+  
+    let displayedText = "";
+  
+    const handleClick = (event) => {
+      displayedText = event.detail.value;
+    };
+  </script>
+  
+  <main class="p-4">
+    <h1 class="text-2xl font-bold mb-4">Click to Display Input Text</h1>
+    <Input placeholder="Enter text here" on:click={handleClick} />
+    {#if displayedText}
+      <p class="mt-4 text-lg">{displayedText}</p>
+    {/if}
+  </main>
+  
