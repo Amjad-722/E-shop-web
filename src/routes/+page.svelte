e4c3c7bbc4fbdf80d59bc7a374c81f81bcdefee7
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-06 16:20:59 +0500

    p

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@ import Navbar from "../components/Navbar/Navbar.svelte";
 <main>
   <section class="h-screen max-h-full bg-cover  bg-center" style="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20230613/pngtree-some-green-plants-and-leaves-against-a-dark-background-image_2899643.jpg');">
     <Navbar />
-    <Home />
+    <Home/>
   </section>
   
   
