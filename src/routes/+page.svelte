d06a415ca6bfb70f20bcd032fefd0016cf1d8c7e
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-06 13:38:24 +0500

    updated the files sect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,6 @@
 <script>
-  import Input from '../components/Input.svelte';
 
-  let inputValue = "";
-  let displayedText = "";
-
-  const handleSubmit = (event) => {
-    event.preventDefault(); 
-    const formData = new FormData(event.target);
-    console.log([...formData]); 
-
-    displayedText = formData.get('inputField'); 
-    event.target.reset(); 
-  };
 </script>
-
-<main class="p-4">
-  <h1 class="text-2xl font-bold mb-4">Click to Display Input Text</h1>
-  <form on:submit={handleSubmit}>
-    <Input 
-      placeholder="Enter text here"
-      bind:value={inputValue}
-      name="inputField" 
-    />
-   
-  </form>
-  {#if displayedText}
-    <p class="mt-4 text-lg">You entered: {displayedText}</p>
-  {/if}
+<main>
+  <h1 class="flex items-center font-extrabold text-6xl justify-center  ">helllo</h1>
 </main>
