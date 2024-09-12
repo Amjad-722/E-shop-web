ce9aafc8b6eda92567718d2b0c065fbc0222f9c3
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-12 19:13:59 +0500

    updated'c

--- src/components/help/Help.svelte
+++ src/components/help/Help.svelte
@@ -1,38 +1,41 @@
 <script>
-    import { faFacebook, faTwitter, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+  import { faFacebook, faTwitter, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
   import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
-  
-  
-    export let images = [];
-    export let title = "Title here";
-    export let subtitle = "Subtitle here";
-    export let description = "Description here";
-    export let subheading = "Subheading here";
-  
-    // Function to set the data
-    const setData = () => {
-      images = [
-        "https://websitedemos.net/plant-shop-04/wp-content/uploads/sites/160/2020/07/green-leaf-plants.jpg",
-        "https://websitedemos.net/plant-shop-04/wp-content/uploads/sites/160/2020/07/woman-planting-plant-in-pot.jpg",
-      ];
-  
-      title = "Nonummy Cum Convallis";
-      subtitle = "We Help Gardens Thrive!";
-      description = `Elementum tristique mi montes nibh deserunt magna. Netus assumenda 
-                        molestias nisi porro, nascetur molas. Habitant urna fugiat wisi nceptos 
-                        sodales magna. Molestie suspendisse faucibus justo! Sollicitudin tempore 
-                        unde, dolorem, blanditiis a nam beatae nihil bibendum, taciti.`;
-      subheading =
-        "Tempore ut mauris aenean cillum ultrices nunc aliqu auris aenean mpore utma.";
-    };
-  
-    setData();
-  </script>
-  
-  <section
-    class="h-dvh flex flex-row lg:flex-row items-center justify-center gap-8 lg:gap-16 "
-  >
+  import Button from '../button/Button.svelte';
+
+  export let images = [];
+  export let title = "Title here";
+  export let subtitle = "Subtitle here";
+  export let description = "Description here";
+  export let subheading = "Subheading here";
+
+  // Function to set the data
+  const setData = () => {
+    images = [
+      "https://websitedemos.net/plant-shop-04/wp-content/uploads/sites/160/2020/07/green-leaf-plants.jpg",
+      "https://websitedemos.net/plant-shop-04/wp-content/uploads/sites/160/2020/07/woman-planting-plant-in-pot.jpg",
+    ];
+
+    title = "Nonummy Cum Convallis";
+    subtitle = "We Help Gardens Thrive!";
+    description = `Elementum tristique mi montes nibh deserunt magna. Netus assumenda 
+                      molestias nisi porro, nascetur molas. Habitant urna fugiat wisi nceptos 
+                      sodales magna. Molestie suspendisse faucibus justo! Sollicitudin tempore 
+                      unde, dolorem, blanditiis a nam beatae nihil bibendum, taciti.`;
+    subheading = "Tempore ut mauris aenean cillum ultrices nunc aliqu auris aenean mpore utma.";
+  };
+
+  setData();
+
+  function handleClick() {
+    alert("Button clicked!");
+  }
+</script>
+
+<section class="h-dvh flex flex-row lg:flex-row items-center justify-center gap-8 lg:gap-16">
+  <div class="flex flex-col">
     <div class="flex flex-row gap-12">
+      <!-- Images Section -->
       <div class="flex flex-row pl-2">
         {#each images as image}
           <img
@@ -41,46 +44,34 @@
             class="w-[250px] h-[350px] sm:w-[300px] sm:h-[600px] md:w-[350px] md:h-[700px] lg:w-[450px] lg:h-[700px] object-cover"
           />
         {/each}
-
-        
       </div>
-  
-     <div>
+
+      <!-- Text and Social Media Section -->
+      <div>
         <div class="flex flex-col justify-center lg:text-left">
-            <h6 class="text-lg font-bold pt-20 text-green-700 mb-2">{subtitle}</h6>
-            <h1 class="text-3xl sm:text-4xl pt-4 lg:text-4xl font-bold leading-4 mb-4">{title}</h1>
-      
-            <p class="text-base text-xs pt-3 sm:text-lg mb-4 max-w-lg lg:max-w-2xl">
-              {description}
-            </p>
-      
-            <h3 class="text-lg pt-10 sm:text-xl font-medium">{subheading}</h3>
-            <img class="h-24 w-24 pt-8" src="https://websitedemos.net/plant-shop-04/wp-content/uploads/sites/160/2018/01/pic55-free-img.png" alt="">
-          </div>
-          
-          <!-- Social Media Icons -->
-          <div class="flex space-x-4 mt-12">
-            <!-- Facebook Icon -->
-            <a href="https://www.facebook.com" target="_blank" class="text-green-700 rounded-full hover:bg-gray-400 ">
-              <FontAwesomeIcon  icon={faFacebook} class=" fa-2x " />
-            </a>
-            
-            <!-- Twitter Icon -->
-            <a href="https://www.twitter.com" target="_blank" class="text-green-700 rounded-full hover:bg-gray-400 ">
-              <FontAwesomeIcon icon={faTwitter} class="fa-2x" />
-            </a>
-      
-            <!-- YouTube Icon -->
-            <a href="https://www.youtube.com" target="_blank" class="ttext-green-700 rounded-full hover:bg-gray-400 ">
-              <FontAwesomeIcon icon={faYoutube} class="fa-2x" />
-            </a>
-      
-            <!-- Instagram Icon -->
-            <a href="https://www.instagram.com" target="_blank" class="text-green-700 rounded-full hover:bg-gray-400 ">
-              <FontAwesomeIcon icon={faInstagram} class="fa-2x" />
-            </a>
-          </div>
-     </div>
+          <h6 class="text-lg font-bold pt-20 text-green-700 mb-2">{subtitle}</h6>
+          <h1 class="text-3xl sm:text-4xl pt-4 lg:text-4xl font-bold leading-4 mb-4">{title}</h1>
+          <p class="text-base text-xs pt-3 sm:text-lg mb-4 max-w-lg lg:max-w-2xl">{description}</p>
+          <h3 class="text-lg pt-10 sm:text-xl font-medium">{subheading}</h3>
+          <img class="h-24 w-24 pt-8" src="https://websitedemos.net/plant-shop-04/wp-content/uploads/sites/160/2018/01/pic55-free-img.png" alt="">
+        </div>
+
+        <!-- Social Media Icons -->
+        <div class="flex space-x-4 mt-12">
+          <a href="https://www.facebook.com" target="_blank" class="text-green-700 hover:bg-gray-400 rounded-full">
+            <FontAwesomeIcon icon={faFacebook} class="fa-2x" />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" class="text-green-700 hover:bg-gray-400 rounded-full">
+            <FontAwesomeIcon icon={faTwitter} class="fa-2x" />
+          </a>
+          <a href="https://www.youtube.com" target="_blank" class="text-green-700 hover:bg-gray-400 rounded-full">
+            <FontAwesomeIcon icon={faYoutube} class="fa-2x" />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" class="text-green-700 hover:bg-gray-400 rounded-full">
+            <FontAwesomeIcon icon={faInstagram} class="fa-2x" />
+          </a>
+        </div>
+      </div>
     </div>
-  </section>
-  
+  </div>
+</section>
---
+++ src/components/help/plantsShop.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Button from "../button/Button.svelte";
+  
+    function handleClick() {
+      alert("Button clicked!");
+    }
+  </script>
+  
+  <div>
+    <div class="flex flex-row justify-around pb-8 items-center align-center">
+      <div class="bg-cover bg-center flex pl-2 py-4 justify-center flex-col text-left h-80 w-1/2 flex-shrink-0" 
+           style="background-image: url('https://www.saferbrand.com/media/Articles/Safer-Brand/26-best-indoor-plants.jpg');">
+        <h1 class="font-bold text-2xl text-white">Plants Care Instruction</h1>
+        <p class="text-white">Each order includes a small care guide booklet.</p>
+        <Button class="mt-6 w-32" on:click={handleClick}>Shop Now</Button>
+      </div>
+  
+      <div class="bg-cover bg-center flex pl-2 py-4 justify-center flex-col text-left h-80 w-1/2 flex-shrink-0" 
+           style="background-image: url('https://www.saferbrand.com/media/Articles/Safer-Brand/26-best-indoor-plants.jpg');">
+        <h1 class="font-bold text-2xl text-white">Plants Care Instruction</h1>
+        <p class="text-white">Each order includes a small care guide booklet.</p>
+        <Button class="" on:click={handleClick}>Shop Now</Button>
+      </div>
+    </div>
+  </div>
+  
+  
