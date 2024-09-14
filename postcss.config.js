e17c1402035040a3f9e93c94f2302bd1129ca57d
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-14 17:25:53 +0500

    updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+module.exports = {
+  theme: {
+    extend: {},
+    container: {
+      center: true, // Centers the container horizontally
+      padding: {
+        DEFAULT: '1rem', // Padding for all screen sizes
+        sm: '2rem', // Padding for small screens
+        lg: '4rem', // Padding for large screens
+        xl: '5rem', // Padding for extra-large screens
+      },
+    },
   },
-}
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+};
