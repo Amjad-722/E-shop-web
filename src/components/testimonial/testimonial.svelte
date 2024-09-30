310dbba6b6c27449eb033ca57b3d66b13d7d7e83
Amjad-722 <aliamjad722ee@gmail.com>
2024-09-30 17:48:15 +0500

    updated

--- src/components/testimonial/testimonial.svelte
+++ src/components/testimonial/testimonial.svelte
@@ -42,7 +42,7 @@
     </div>
   
    
-    <div class="relative grid grid-cols-1 md:grid-cols-2 gap-8 px-12 z-10 justify-items-center">
+    <div class="relative grid grid-cols-1 md:grid-cols-2 gap-8 px-12 z-10 max-w-7xl mx-auto">
       {#each testimonials as testimonial}
         <div
           class="border-2 border-gray-300 hover:border-blue-500 transition hover:translate-x-3 duration-300 p-6 rounded-md shadow-md bg-white bg-opacity-80  w-full"
